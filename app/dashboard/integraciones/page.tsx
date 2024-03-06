ec45f51902f5ba7d772e09da7eea00a8615648a4
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-03-05 22:37:25 -0300

    mercado pago message added

--- app/dashboard/integraciones/page.tsx
+++ app/dashboard/integraciones/page.tsx
@@ -3,6 +3,7 @@ import MercadoPagoForm from "../mercado-pago-form/mercado-pago-form";
 import { getUserByEmail } from "@/lib/api/users";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default async function Integraciones() {
   const session = await getServerSession(authOptions);
@@ -12,6 +13,15 @@ export default async function Integraciones() {
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Integraciones
       </h1>
+      {!user.mpAccessToken && (
+        <Alert variant="destructive">
+          <AlertTitle>Atención!</AlertTitle>
+          <AlertDescription>
+            Es necesario que completes tus datos de MercadoPago para poder
+            cobrar.
+          </AlertDescription>
+        </Alert>
+      )}
       <h2 className="mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
         Mercado Pago
       </h2>

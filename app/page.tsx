d85b0e12662aabf9ac1ab5888f5e68ef19b75ea0
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-03-06 23:27:10 -0300

    wip skeletons

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { HomeCard } from "@/types/card";
 
 export default async function Home() {
   const eventos = await getAllEvents();
-  
+
   return (
     <section>
       <h1 className="mb-14 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">

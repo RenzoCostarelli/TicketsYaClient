6deb8e6de649ac215eeefbcd62a593b458d4acbe
Nicolas <rojonicolasdev@gmail.com>
2024-03-08 13:36:54 -0300

    Refactor edit ticket type form to handle free tickets

--- app/dashboard/components/edit-ticket-type-form/edit-ticket-type-form.tsx
+++ app/dashboard/components/edit-ticket-type-form/edit-ticket-type-form.tsx
@@ -175,7 +175,6 @@ export default function EditTycketTypeForm({
             </FormItem>
           )}
         />
-
         <FormField
           control={form.control}
           name="selectedDates"

5dabc2a81e50fecb5ea967c9ceaa3f8faef1117a
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-03-12 22:54:17 -0300

    WIP: notifications and disc code

--- app/dashboard/configuracion/notificaciones/page.tsx
+++ app/dashboard/configuracion/notificaciones/page.tsx
@@ -1,6 +1,13 @@
+import { getServerSession } from "next-auth";
 import NotificationsForm from "../../components/notifications-form/notifications-form";
+import { getUserByEmail } from "@/lib/api/users";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getAllNotifications } from "@/lib/actions";
 
-export default function Notificaciones() {
+export default async function Notificaciones() {
+  const session = await getServerSession(authOptions);
+  const { id } = await getUserByEmail(session?.user?.email as string);
+  const notifications = (await getAllNotifications(id)) || [];
   return (
     <div>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
@@ -9,7 +16,7 @@ export default function Notificaciones() {
       <p className="text-muted-foreground">
         Elegí que notificaciones recibir en tu e-mail.
       </p>
-      <NotificationsForm />
+      <NotificationsForm notifications={notifications} />
     </div>
   );
 }

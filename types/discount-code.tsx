eb3bf6cccdb7642fcf1ae16753ff0e99b8a9c5be
Nicolas <rojonicolasdev@gmail.com>
2024-03-15 15:18:41 -0300

    feat: Add DiscountCode type and GlobalStatus enum in discount-code.tsx

---
+++ types/discount-code.tsx
@@ -0,0 +1,10 @@
+export type GlobalStatus = "DRAFT" | "ACTIVE" | "CONCLUDED" | "DELETED";
+
+export interface DiscountCode {
+  id?: string;
+  eventId?: string;
+  ticketTypeId?: string;
+  code: string;
+  expiresAt: Date;
+  status: GlobalStatus;
+}

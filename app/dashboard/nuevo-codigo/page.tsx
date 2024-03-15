c6d3c1f07aa8b0d1fc250324fc325ca45fc4e180
Nicolas <rojonicolasdev@gmail.com>
2024-03-15 15:19:11 -0300

    Added new page for creating discount codes in dashboard

---
+++ app/dashboard/nuevo-codigo/page.tsx
@@ -0,0 +1,28 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getUserByEmail } from "@/lib/api/users";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
+import CreateCodeForm from "../components/create-discount-code/create-discount-code";
+
+export default async function NewEvent() {
+  const session = await getServerSession(authOptions);
+  const { id } = await getUserByEmail(session?.user?.email as string);
+  return (
+    <>
+      <Button asChild variant="ghost">
+        <Link href="/dashboard/codigos">
+          <ChevronLeft /> Voler al panel
+        </Link>
+      </Button>
+
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Nuevo código
+      </h1>
+
+      <CreateCodeForm userId={id} />
+    </>
+  );
+}

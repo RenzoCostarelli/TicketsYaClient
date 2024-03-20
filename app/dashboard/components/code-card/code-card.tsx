ac3b78bb7d4ce1bfef833d9fae5ef2949af0d47c
Nicolas <rojonicolasdev@gmail.com>
2024-03-20 10:14:15 -0300

    feat(client): Implement CodeCard component for displaying discount codes

--- app/dashboard/components/code-card/code-card.tsx
+++ app/dashboard/components/code-card/code-card.tsx
@@ -8,26 +8,42 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import {
-  BarChartIcon,
   FileEditIcon,
-  KeyIcon,
-  TicketIcon,
   TrashIcon,
 } from "lucide-react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
-import { deleteEvent } from "@/lib/actions";
+import { deleteDiscountCode, getEventById } from "@/lib/actions";
 import { toast } from "@/components/ui/use-toast";
 import { DiscountCode } from "@/types/discount-code";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
 
-export default function CodeCard({ discountCode }: { discountCode: DiscountCode }) {
-  const handleDeleteEvent = () => {
-    deleteEvent(discountCode.id)
+export default function CodeCard({
+  discountCode,
+}: {
+  discountCode: DiscountCode,
+}) {
+  
+  const getEventTitle = async (eventId: string) => {
+    const evento = await getEventById(eventId);    
+    return evento?.title || '';
+  };
+
+  const [eventTitle, setEventTitle] = useState<string>("");
+
+  useEffect(() => {
+    getEventTitle(discountCode.eventId as string)
+      .then(title => setEventTitle(title))
+      .catch(error => console.error("Error obteniendo el título del evento:", error));
+  }, [discountCode.eventId]);
+  
+  const handleDeleteCode = () => {
+    deleteDiscountCode(discountCode.id as string)
       .then(() => {
         toast({
-          title: "Evento borrado!",
+          title: "Código borrado!",
         });
       })
       .catch((error: string) => {
@@ -41,12 +57,16 @@ export default function CodeCard({ discountCode }: { discountCode: DiscountCode 
 
   return (
     <>
-      <Card className="w-full max-w-xl" key={discountCode.id}>
-        <CardHeader className="flex flex-row items-center gap-2">          
+      <Card className="w-full max-w-xl" key={ discountCode.id }>
+        <CardHeader className="flex flex-row items-center gap-2">
           <div className="grid gap-1">
-            <CardTitle>{discountCode.code}</CardTitle>
-           {/* <CardDescription>{discountCode.description}</CardDescription>*/}
-          </div>discountCode
+            <CardTitle>Código de descuento</CardTitle>
+            <CardDescription>{ discountCode.code }</CardDescription>
+            <CardDescription>Evento: { eventTitle }</CardDescription>
+            <CardDescription>
+              Expira: {format(discountCode.expiresAt, "dd/MM/yyyy")}
+            </CardDescription>
+          </div>
         </CardHeader>
         <CardFooter className="flex justify-start gap-4">
           <Link href={`/dashboard/codigos/${discountCode.id}`}>
@@ -57,7 +77,7 @@ export default function CodeCard({ discountCode }: { discountCode: DiscountCode 
             className="text-red-500"
             size="icon"
             variant="ghost"
-            onClick={handleDeleteEvent}
+            onClick={handleDeleteCode}
           >
             <TrashIcon className="w-6 h-6" />
             <span className="sr-only"> Eliminar </span>

f31915f0870811158827f7e66e3344d847b3b1f3
Nicolas <rojonicolasdev@gmail.com>
2024-03-20 10:13:39 -0300

    feat(client): Implement discount code editing page

--- app/dashboard/codigos/[id]/page.tsx
+++ app/dashboard/codigos/[id]/page.tsx
@@ -1,17 +1,25 @@
 import { getDiscountCodeById } from "@/lib/actions";
-
+import { getEventsByUserId } from "@/lib/api/eventos";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
 import EditCodeForm from "@/app/dashboard/components/edit-discount-code/edit-discount-code-form";
 import { DiscountCode } from "@/types/discount-code";
+import { Evento } from "@/types/event";
+import { getUserByEmail } from "@/lib/api/users";
 
 export default async function EditCode({
   params,
 }: {
-  params: { id: string };
+  params: { id: string },
 }) {
+  const session = await getServerSession(authOptions);
+  const { id } = await getUserByEmail(session?.user?.email as string);
   const discountCode = await getDiscountCodeById(params.id);
+  const events = await getEventsByUserId(id);
+
   return (
     <>
       <Button asChild variant="ghost">
@@ -22,7 +30,7 @@ export default async function EditCode({
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         Editar código
       </h1>
-      <EditCodeForm discountCode={discountCode as DiscountCode} />
+      <EditCodeForm discountCode={discountCode as DiscountCode} events={ events as Evento[]}/>
     </>
   );
 }
